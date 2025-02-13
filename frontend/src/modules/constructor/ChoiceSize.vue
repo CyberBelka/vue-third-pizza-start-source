426b0d29b13ab74b10845be7ca39507c7ac6c87c
Belka <vfrosenyuk@gmail.com>
2025-02-13 11:37:37 +0400

    added counter component

--- frontend/src/modules/constructor/ChoiceSize.vue
+++ frontend/src/modules/constructor/ChoiceSize.vue
@@ -38,3 +38,77 @@ const emit = defineEmits(["update:modelValue"]);
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/ds-system/ds.scss";
+@import "@/assets/scss/mixins/mixins.scss";
+
+.diameter {
+  &__input {
+    margin-right: 8.7%;
+    margin-bottom: 20px;
+    padding-top: 7px;
+    padding-bottom: 6px;
+
+    cursor: pointer;
+
+    span {
+      @include r-s16-h19;
+
+      position: relative;
+
+      padding-left: 46px;
+
+      &::before {
+        @include p_center_v;
+
+        width: 36px;
+        height: 36px;
+
+        content: "";
+        transition: 0.3s;
+
+        border-radius: 50%;
+        background-color: $green-100;
+        background-image: url("@/assets/img/diameter.svg");
+        background-repeat: no-repeat;
+        background-position: center;
+      }
+    }
+
+    &:nth-child(3n) {
+      margin-right: 0;
+    }
+
+    &--small {
+      span::before {
+        background-size: 18px;
+      }
+    }
+
+    &--normal {
+      span::before {
+        background-size: 29px;
+      }
+    }
+
+    &--big {
+      span::before {
+        background-size: 100%;
+      }
+    }
+
+    &:hover {
+      span::before {
+        box-shadow: $shadow-regular;
+      }
+    }
+
+    input {
+      &:checked + span::before {
+        box-shadow: $shadow-large;
+      }
+    }
+  }
+}
+</style>
