1890be376116cc92805e67a0c9f3f284480feb03
Belka <vfrosenyuk@gmail.com>
2025-04-09 09:58:29 +0400

    refactoring

--- frontend/src/layouts/AppLayoutLogin.vue
+++ frontend/src/layouts/AppLayoutLogin.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    <app-header />
     <slot />
   </div>
 </template>
 
 <script setup>
 import { useRoute } from "vue-router";
-import AppHeader from "./AppHeader.vue";
-
-const route = useRoute();
 </script>
